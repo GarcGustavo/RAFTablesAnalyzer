922413272a2458fc199bbdd37b31cdc4240801f4
Gustavo Reyes <gustavo.reyes1@upr.edu>
2016-10-09 03:33:11 -0400

    implemented char classes, untested and probably wrong

--- src/dataManagementClasses/CharDataReader.java
+++ src/dataManagementClasses/CharDataReader.java
@@ -1,20 +1,27 @@
 package dataManagementClasses;
 
+import interfaces.DataReader;
+
 import java.util.Scanner;
 
 public class CharDataReader implements DataReader {
-	private IntDataReader() {}; 
+	private static final int CHARSIZE = Character.SIZE; 
+	public static final CharDataReader INSTANCE = new CharDataReader(); 
 	
+	private CharDataReader() {}; 
+	
+	//Need to fix this, look up how to tell chars from bits
 	public Character readDataFromArrayOfBytes(byte[] b, int index) {
 		int value = 0; 
 		int lSB; 
-		for (int i=0; i < INTSIZE; i++) { 
+		for (int i=0; i < CHARSIZE; i++) { 
 			value = value << 8; 
 			lSB = 0x000000ff & b[index + i];
 			value = value | lSB; 
 		}
 		return value; 
 	}
+	
 	
 	@Override
 	public Object readDataFromInputScanner(Scanner input) {
--- src/dataManagementClasses/CharDataWriter.java
+++ src/dataManagementClasses/CharDataWriter.java
@@ -1,5 +1,25 @@
 package dataManagementClasses;
 
+import generalUtilities.DataUtils;
+import interfaces.DataWriter;
+
 public class CharDataWriter implements DataWriter{
+	
+	private static final int CHARSIZE = Character.SIZE; 
+	public static final CharDataWriter INSTANCE = new CharDataWriter(); 
+	
+	private CharDataWriter() {}; 
+
+	//Need to test/fix writer method
+	@Override
+	public void writeDataToArrayOfBytes(byte[] a, int starting, Object rvalue) {
+		Character value = (Character) rvalue; 
+		a[starting] = (byte) (value.charValue());  
+	}
+
+	@Override
+	public String toString(Object value) {
+		return String.format(DataUtils.CHARFORMAT, (Character) value); 
+	}
 
 }
